cbc8a963e6e1da1004ae0fd33e819983d0eba7c3
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-02-06 02:05:57 +0530

    Create array_operation.cpp

---
+++ array_operation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main(){
+    array<int,5>a1={2,6,7,3,1};
+    cout<<"size of array "<<a1.size()<<endl;;
+    for(int i=0;i<5;i++){
+        cout<<a1[i]<<" ";
+        cout<<endl;
+    }
+    cout<<"first element of array "<<a1.front()<<endl;
+    cout<<"last element of array "<<a1.back()<<endl;
+    cout<<"is array empty "<<a1.empty()<<endl;
+    return 0;
+}
